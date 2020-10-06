0c9166edffccb83bd3dd41d9524f6479528c5e25
priyas9595 <59990900+priyas9595@users.noreply.github.com>
2020-10-06 22:19:27 +0530

    add mongoose connection

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import mongoose from 'mongoose';
 const app = express();
 const PORT = 4000;
+var mongoDB = 'mongodb://127.0.0.1/productsdb';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 
+//Get the default connection
+var productsdb  = mongoose.connection;
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
