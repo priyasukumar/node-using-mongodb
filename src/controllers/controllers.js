7764c0269e794c26062a9cc7d530a24a8235a170
priyas9595 <59990900+priyas9595@users.noreply.github.com>
2020-10-08 22:23:42 +0530

    update and delete api

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -24,12 +24,34 @@ export const getProducts = (req, res) => {
 
 };
 export const getProductWithID = (req, res) => {
-    
+
     Product.findById(req.params.ProductID, (err, Product) => {
         if (err) {
             res.send(err);
         }
         res.json(Product);
+    });
+
+
+};
+export const updateProduct = (req, res) => {
+
+    Product.findOneAndUpdate({ _id: req.params.ProductID }, req.body, { new: true, useFindAndModify: false }, (err, Product) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(Product);
+    });
+
+
+};
+export const deleteProduct = (req, res) => {
+
+    Product.deleteOne({ _id: req.params.ProductID}, (err, Product) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json({ message: 'successfully deleted product' });
     });
 
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,11 @@
-import { addnewProduct, getProducts, getProductWithID } from '../controllers/controllers'
+import { addnewProduct, getProducts, getProductWithID, updateProduct, deleteProduct } from '../controllers/controllers'
 export const routes = (app) => {
     app.route('/products')
         .post(addnewProduct)
         .get(getProducts);
     app.route('/products/:ProductID')
-        .get(getProductWithID);
+        .get(getProductWithID)
+        .put(updateProduct)
+        .delete(deleteProduct);
 
 }
